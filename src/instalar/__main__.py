c7ae26706fba20a326e5580bda4c7e9f0271ec95
Jek <jchoo@splunk.com>
2025-02-20 23:29:37 -0800

    feat: add init support for windows

--- src/instalar/__main__.py
+++ src/instalar/__main__.py
@@ -1,7 +1,15 @@
 from instalar.client.console import main
 
-def run():
+# For others
+def main_cli():
     exit(main())
 
+# For windows. On Windows, scripts packaged this way need a terminal,
+# so if you launch them from within a graphical application, they will make a terminal pop up.
+# To prevent this from happening, use the [project.gui-scripts] table instead of [project.scripts].
+# https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#creating-executable-scripts
+# def main_gui():
+#     main()
+
 if __name__ == '__main__':
-    run()
+    main_cli()
